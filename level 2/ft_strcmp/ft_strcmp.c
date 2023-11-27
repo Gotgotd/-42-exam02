e2095b07a2f3e5e7f5bcb5ef26cf669db0df5a58
Gotgotd <115143559+Gotgotd@users.noreply.github.com>
2023-11-27 15:50:03 +0100

    Update ft_strcmp.c

--- level 2/ft_strcmp/ft_strcmp.c
+++ level 2/ft_strcmp/ft_strcmp.c
@@ -1,13 +1,4 @@
 
-/***
- Reproduce the behavior of the function strcmp (man strcmp).
-
-Your function must be declared as follows:
-
-int    ft_strcmp(char *s1, char *s2);
-
-***/
-
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i = 0;
